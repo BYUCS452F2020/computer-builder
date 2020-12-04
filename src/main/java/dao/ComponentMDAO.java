0a2b0724b06e0bcae660a5e26c40ed47771bdbf5
jackz <jackzcxc137@gmail.com>
2020-12-03 23:51:10 -0700

    More clean up

--- src/main/java/dao/ComponentMDAO.java
+++ src/main/java/dao/ComponentMDAO.java
@@ -9,19 +9,10 @@ import static com.mongodb.client.model.Filters.*;
 import models.Component;
 import org.bson.Document;
 
-import javax.print.Doc;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ComponentMDAO {
-    /*
-        - Need to establish the correct connection to the mongo database
-        - Have insert method that is only used for data admin
-        - Have find method for singular component and one for the conditional finds
-        - Maybe include an update function
-        - Query document will consist of at least type and then performance, price, and tdp
-        - Structure of the collection will be all components in one collection identified by type.
-     */
 
     public void insert(Component component) throws DataAccessException {
 
